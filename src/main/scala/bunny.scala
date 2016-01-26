77a4d2f96a23728f8c1b7af67e86e377c61f78ac
Remi Oudin <oudin@crans.org>
2016-01-26 18:08:06 +0100

    Heavy Bunny

--- src/main/scala/bunny.scala
+++ src/main/scala/bunny.scala
@@ -3,10 +3,20 @@ package runtime
 
 import path._
 
+/* Creates an abstract class of bunnies. Then every species of bunny is a
+subclass of this class */
 abstract class Bunny {
   val hp = 10
   val x = 2.
   val y = 2.
   val speed = 1.
+  val path = new Path
+}
+
+class Heavy_Bunny extends Bunny {
+  val hp = 20
+  val x = 2.
+  val y = 2.
+  val speed = 0.5
   val path = new Path
 }
