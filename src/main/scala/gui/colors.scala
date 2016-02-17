d02cb210b69eebe63e80b8aec1508814758c175d
Kevin Le run <le-run@crans.org>
2016-02-17 18:38:45 +0100

    [colors.scala] added a couple of light colors

--- src/main/scala/gui/colors.scala
+++ src/main/scala/gui/colors.scala
@@ -5,11 +5,16 @@ import swing.Color
 
 object Colors {
 
-  val white     = new Color( 255, 255, 255 )
-  val lightGrey = new Color( 200, 200, 200 )
-  val black     = new Color(   0,   0,   0 )
-  val red       = new Color( 255,   0,   0 )
-  val lightred  = new Color( 255, 200, 200 )
-  val blue      = new Color(   0,   0, 255 )
-  val green     = new Color(   0, 255,   0 )
+  val white      = new Color( 255, 255, 255 )
+  val lightGrey  = new Color( 200, 200, 200 )
+  val black      = new Color(   0,   0,   0 )
+
+  val red        = new Color( 255,   0,   0 )
+  val lightred   = new Color( 255, 200, 200 )
+
+  val green      = new Color(   0, 255,   0 )
+  val lightgreen = new Color( 200, 255, 200 )
+
+  val blue       = new Color(   0,   0, 255 )
+  val lightblue  = new Color( 200, 200, 255 )
 }
