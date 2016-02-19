ebadd7ccb21acc211066b38fb31773378a2a1c32
Diffrac <alexis.laouar@ens-cachan.fr>
2016-02-19 20:03:56 +0100

    [resources] Automatic wave generation done (not very well balanced for now, reaaally hard to beat)

--- src/main/resources/waves/wave_generation/wave_gen.ml
+++ src/main/resources/waves/wave_generation/wave_gen.ml
@@ -16,22 +16,35 @@ let scd (x,y,z,t) = y;;
 let trd (x,y,z,t) = z;;
 let frt (x,y,z,t) = t;;
 
-let n_wave = 5;;
-let difficulty = 20;;
-let bunnies = [|"Bunny",1,200,1 ; "Heavy_Bunny",3,50,1 ; "Hare",1,35,3 ; "Otter",15,10,10|]
-(* List of (bunny type, difficulty points, inverse rarity, first possible wave of appearance) *)
-let sum_rarity =
+
+Random.init (int_of_float(Unix.gettimeofday ()));;
+  
+let random_int n = Random.int n;;
+  
+let n_wave = int_of_string(Sys.argv.(1));;
+let difficulty = 20 + n_wave*n_wave;;
+let bunnies = [|"Bunny",1,200,1 ; "HeavyBunny",3,50,1 ; "Hare",1,35,3|];;
+let bosses = [|"Otter",15,10,10|];;
+(* bunnies and bosses : list of (bunny type, difficulty points, inverse rarity, first possible wave of appearance) *)
+let sum_rarity_bunn =
   let res = ref 0 in
   for i=0 to Array.length(bunnies)-1 do
     res := (!res) + (trd bunnies.(i))
   done;
   !res;;
-
-let file_name = "wave_" ^ (string_of_int n_wave);;
+  
+let sum_rarity_boss =
+  let res = ref 0 in
+  for i=0 to Array.length(bosses)-1 do
+    res := (!res) + (trd bosses.(i))
+  done;
+  !res;;
+  
+let file_name = "../wave" ^ (string_of_int n_wave) ^ ".csv";;
 let oc = open_out file_name;;
 
 let chose_bunny i =
-  (* i is a random integer between 0 and sum_rarity-1, choses a random bunny based on rarity *)
+  (* i is a random integer between 0 and sum_rarity_bunn-1, choses a random bunny based on rarity *)
   let j = ref (i - (trd bunnies.(0)))
   and bunny_index = ref 0 in
   while (!j >= 0) && (!bunny_index < Array.length(bunnies)) do
@@ -40,20 +53,44 @@ let chose_bunny i =
   done;
   !bunny_index;;
 
+ let chose_boss i =
+  (* i is a random integer between 0 and sum_rarity_boss-1, choses a random boss based on rarity *)
+  let j = ref (i - (trd bosses.(0)))
+  and boss_index = ref 0 in
+  while (!j >= 0) && (!boss_index < Array.length(bosses)) do
+    incr boss_index;
+    j:= (!j) - (trd bosses.(!boss_index))
+  done;
+  !boss_index;;
 
 let rec wave n t=
-  (* Prints a wave corresponding to the difficulty n in file wave_{n_wave}, starting at time t *)
+  (* Prints a NON BOSS wave corresponding to the difficulty n in file wave_{n_wave}, starting at time t *)
   match n with
   |n when n <= 0
       -> ()
-  |n  -> let bunny = bunnies.(chose_bunny (Random.int sum_rarity)) in
-	let diff_decr = ref 0 in 
-	if n_wave >= frt bunny then
-	  begin
-	    let diff = scd bunny in
-	    fprintf oc "%f, %s\n" t (frs bunny);
-	    diff_decr := diff
-	  end;
-	wave (n-(!diff_decr)) (t+.1.);;
-
-wave difficulty 0.;;
+  |n  ->  let bunny = bunnies.(chose_bunny (random_int sum_rarity_bunn)) in
+	  let diff_decr = ref 0 in 
+	  if n_wave >= frt bunny then
+	    begin
+	      let diff = scd bunny in
+	      fprintf oc "%f, %s\n" t (frs bunny);
+	      diff_decr := diff
+	    end;
+	  wave (n-(!diff_decr)) (t+.1.);;
+  
+  
+let print_wave n t=
+  let r = random_int 100 in
+  if (n_wave >= 10) && (r < 6) then
+    (* BOSS TIME, B*TCHES *)
+    begin
+      let boss=bosses.(chose_boss (random_int sum_rarity_boss)) in
+      fprintf oc "%f, %s\n" t (frs boss)
+    end
+  else
+    (* No boss for now... *)
+    wave n t;;
+  
+  
+print_wave difficulty 0.;;
+    
---
+++ src/main/resources/waves/wave_generation/wave_gen.sh
@@ -0,0 +1,1 @@
+for i in {1..30}; do ./wave_gen $i; done;
