065ceba8d01da167327e85f33cf425420d291f10
Kevin Le run <le-run@crans.org>
2016-03-11 16:28:25 +0100

    [gui] Made gold animations fade

--- src/main/scala/gui/animatable.scala
+++ src/main/scala/gui/animatable.scala
@@ -1,6 +1,7 @@
 
 package gui
 
+import java.awt.AlphaComposite
 import java.awt.Graphics2D
 import java.awt.image.BufferedImage
 import java.io.File
@@ -43,12 +44,15 @@ abstract class Animatable
 
 class GoldAnimation(amount: Int,origin: Waypoint) extends Animatable
 {
+    timer = 2.0
     var pos    = origin
     val target = origin + new Waypoint(0,-1)
 
     override def draw(g: Graphics2D): Unit = {
         pos = origin * timer + target * (1 - timer)
         val string = "+" + amount.toString + " Gold"
+        val alpha  = if(timer < 1.0) { timer.toFloat } else { 1.0f }
+        g.setComposite( AlphaComposite.getInstance( AlphaComposite.SRC_OVER, alpha ) );
         g.setColor( Colors.black )
         g.drawString( string,
             pos.x.toFloat * MapPanel.cellsize + 1,
