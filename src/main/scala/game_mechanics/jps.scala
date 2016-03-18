787da45b64991eca26aa232c7feda0ee5bb7e872
Rémi Oudin <oudin@crans.org>
2016-03-18 18:26:04 +0100

    [jps.scala] Implementing jps. Created a new class for CellPos with parents

--- src/main/scala/game_mechanics/jps.scala
+++ src/main/scala/game_mechanics/jps.scala
@@ -3,10 +3,27 @@ package game_mechanics
 
 import game_mechanics.path._
 
-import collection.mutable.ListBuffer
+import collection.mutable.{ListBuffer,ListMap,Queue}
 
 
-/* TODO List: add_node, get_closed_node, parent_node */
+
+class CellPosed(cell: CellPos, dir : (Int,Int)) {
+    /**
+     * Jump point, it is a cell with a possible parent
+     * @param cell: the cell
+     * @param parent: the parent or nothing
+     */
+    val cell = cell
+    var parent = None
+    val dir = dir
+    val set_parent(parent : CellPosed) : Unit = {
+        assert (self.parent.isEmpty, {
+            this.parent = parent
+        }
+    }
+}
+
+/* TODO List: add_node, get_closed_node */
 
 class JPS(start: CellPos, objectif: CellPos) {
   val vert_dist = 1
@@ -14,6 +31,71 @@ class JPS(start: CellPos, objectif: CellPos) {
   val diag_dist = (CellPos(1,1) - CellPos(0,0)).norm
   val start     = start
   val objectif  = objectif
+  val all_list: ListMap[CellPosed,Double] = new ListMap()
+  val queue : Queue[(Double,CellPosed,Double)]
+
+
+  for (dx <- [0,1]) {
+      for (dy <- [-1,0,1]) {
+          if ( dy != 0) {
+              this.add_node(this.start.x,this.start.y,(dx,dy),0)
+          }
+      }
+  }
+
+  val estimate(x: Int, y: Int, dir: Option[(Int,Int)]) : Double = {
+      if (dir.isEmpty || dir=(0,0)) {
+          add = 0
+      }
+      else {
+          add = 7
+          x += dir[0]
+          y += dir[1]
+      }
+      val dx = abs(x - this.objectif.x)
+      val dy = abs(y - this.objectif.y)
+      mini =min(dx,dy)
+      return add + 7*mini + 5* ((dx-mini) + (dy-mini))
+  }
+
+
+  val add_node(x: Int, y: Int, dir: (Int,Int), dist: Int): CellPosed = {
+      val pd = CellPosed(Cell(x,y),dir)
+      val current  = this.all_list.get(pd)
+      if ( curren.isEmpty || current > dist) {
+          val total = dist + this.estimate(pd.cell.x, pd.cell.y)
+          this.all_list.update(pd,dist)
+          this.add_open(total,pd,dist)
+      }
+      return pd
+  }
+
+  val get_closed_node(x: Int, y: Int, dir: (Int,Int)) : CellPosed = {
+      val pd = CellPosed(Cell(x,y),dir)
+      val current = this.all_list.get(pd)
+      if (!current.isEmpty && current <= dist) {
+          return current
+      }
+      this.all_list.update(pd,dist)
+      return pd
+ }
+
+ val add_open(total, pd, dist): Unit = {
+      queue += (total,pd,dist)
+ }
+
+ val get_open() : Option[(Double,CellPosed,Double)] = {
+     while (true):
+         if (this.queue.isEmpty) {
+             return None,None,None
+         }
+         total,pd,dist = this.queue.dequeue
+         current = this.all_list.get(pd)
+         if (dist == current) {
+             return Some(total,pd,list)
+         }
+ }
+
 
   val horizontal_search(
     pos: CellPos,
@@ -177,6 +259,7 @@ class JPS(start: CellPos, objectif: CellPos) {
             }
             nodes += parent_node
           }
+        }
 
           if (nodes.isEmpty) {
             val sub_nodes = this.vertical_search(CellPos(x1,y1), vert_dist, dist)
@@ -189,18 +272,50 @@ class JPS(start: CellPos, objectif: CellPos) {
               }
               nodes += parent_node
             }
+          }
 
             if (!nodes.isEmpty) {
               if (!hor_done) {
                 nodes += add_node(x1, y1, (hor_dir, 0), dist))
+              }
+              if (!vert_node) {
+                  nodes += add_node(x1, y1, (0, vert_dir), dist))
+              }
+              nodes += add_node(x1, y1, (hor_dir, vert_dir), dist))
+              return nodes
             }
-            if (!vert_node) {
-              nodes += add_node(x1, y1, (0, vert_dir), dist))
-          }
-          nodes += add_node(x1, y1, (hor_dir, vert_dir), dist))
-        return nodes
-      }
+
 
       x0, y0 = x1, y1
+      }
     }
+
+    val step(dist : Double, cell: CellPosed ): Option[CellPosed] = {
+        if ((elem.cell.x, elem.cell.y) == (this.objectif.x,this.objectif.y)) {
+            return elm
+        }
+
+        val hor_dir  = elm.dir[0]
+        val vert_dir = elm.dir[1]
+
+        if (hor_dir != 0 && vert_dir != 0) {
+            nodes = this.diag_search(elm, hor_dir, vert_dir, dist)
+        }
+
+        else if (vert_dir != 0) {
+            nodes = this.vert_search(elm,vert_dir, dist)
+        }
+        else {
+            assert(hor_dir != 0, {
+                nodes = this.hor_search(elm,hor_dir, dist)
+            }
+            )
+        }
+
+        for node <- nodes
+            node.set_parent(elm)
+
+        return None
+    }
+
 
