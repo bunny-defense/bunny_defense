19ca3b9acc8b7db36cd34cc67fa33b065fd2151e
Diffrac <alexis.laouar@ens-cachan.fr>
2016-03-18 17:14:59 +0100

    [resources] OH MY GOD, AN OTTER SPRITE

--- src/main/scala/game_mechanics/bunny.scala
+++ src/main/scala/game_mechanics/bunny.scala
@@ -58,6 +58,9 @@ object Hare extends BunnyType
 /* A boss! */
 object Otter extends BunnyType
 {
+    override val bunny_graphic =
+        ImageIO.read(
+            new File(getClass().getResource("/mobs/otter.png").getPath()))
     override val initial_hp = 1000.0
     override val shield     = 1.5
     override val speed      = 1.0
