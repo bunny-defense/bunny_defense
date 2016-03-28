a2c90512ff673132eea21559af6926cf4a78c347
Kevin Le run <le-run@crans.org>
2016-03-29 00:20:38 +0200

    [spawner] re-indenting

--- src/main/scala/runtime/spawner.scala
+++ src/main/scala/runtime/spawner.scala
@@ -12,48 +12,50 @@ import runtime._
 
 object Spawner
 {
-  val bunnystart = new CellPos(
-      -1,
-      TowerDefense.map_panel.map.height / 2)
-  val bunnyend   = new CellPos(
-      TowerDefense.map_panel.map.width,
-      TowerDefense.map_panel.map.height / 2)
-  val path =(new JPS( bunnystart, bunnyend )).run() match {
-      case None    => throw new Exception()
-      case Some(p) => p
-  }
+    val bunnystart = new CellPos(
+        -1,
+        TowerDefense.map_panel.map.height / 2)
+    val bunnyend   = new CellPos(
+        TowerDefense.map_panel.map.width,
+        TowerDefense.map_panel.map.height / 2)
+    val path = (new JPS( bunnystart, bunnyend )).run() match
+    {
+        case None    => throw new Exception()
+        case Some(p) => p
+    }
 }
 
 class Spawner(id: Int) {
-  import Spawner._
-  val src = scala.io.Source.fromFile("src/main/resources/waves/wave"+id.toString+".csv")
-  val iter = src.getLines().filter( _ != "" ).map(_.split(","))
-  var spawn_scheduler = new Queue[(Double,BunnyType)]
-  var has_boss = false
+    import Spawner._
+    val src = scala.io.Source.fromFile("src/main/resources/waves/wave"+id.toString+".csv")
+    val iter = src.getLines().filter( _ != "" ).map(_.split(","))
+    var spawn_scheduler = new Queue[(Double,BunnyType)]
+    var has_boss = false
 
-  val law = new Random()
+    val law = new Random()
 
-  val mappage: Map[String, BunnyType] = Map(
-    "Bunny"       -> NormalBunny,
-    "HeavyBunny"  -> HeavyBunny,
-    "Hare"        -> Hare,
-    "Otter"       -> Otter,
-    "GoldenBunny" -> GoldenBunny,
-    "BadassBunny" -> BadassBunny
-  )
-  def create(): Queue[(Double,BunnyType)] = {
-    for (appear <- iter) {
-      if (law.nextDouble > 1.0/1000.0) {
-        val class_name = appear(1).trim
-        if( class_name == "Otter" )
-            has_boss = true
-        spawn_scheduler += (( appear(0).toDouble, mappage(class_name) ))
-      }
-      else {
-        spawn_scheduler += (( appear(0).toDouble, GoldenBunny ))
-      }
+    val mappage: Map[String, BunnyType] = Map(
+        "Bunny"       -> NormalBunny,
+        "HeavyBunny"  -> HeavyBunny,
+        "Hare"        -> Hare,
+        "Otter"       -> Otter,
+        "GoldenBunny" -> GoldenBunny,
+        "BadassBunny" -> BadassBunny
+    )
+
+    def create(): Queue[(Double,BunnyType)] = {
+        for (appear <- iter) {
+            if (law.nextDouble > 1.0/1000.0) {
+                val class_name = appear(1).trim
+                if( class_name == "Otter" )
+                    has_boss = true
+                spawn_scheduler += (( appear(0).toDouble, mappage(class_name) ))
+            }
+            else {
+                spawn_scheduler += (( appear(0).toDouble, GoldenBunny ))
+            }
+        }
+        return(spawn_scheduler)
     }
-    return(spawn_scheduler)
-  }
 
 }
