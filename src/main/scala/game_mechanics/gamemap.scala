79de79bf13473617cf22c215894922d1bbbb2fdc
Kevin Le run <le-run@crans.org>
2016-03-29 01:20:10 +0200

    [gamemap] Forgot to push fixes T.T

--- src/main/scala/game_mechanics/gamemap.scala
+++ src/main/scala/game_mechanics/gamemap.scala
@@ -62,6 +62,8 @@ class GameMap(width0: Int, height0: Int)
     }
 
     def obstructed(x: Int, y: Int) : Boolean = {
+        if( x == -1 || x == this.width )
+            return true
         obstruction_map(x)(y)
     }
 
