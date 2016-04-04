8ec2c652e06cfce6e859ac630349b5789f6a1f8d
Remi Oudin <oudin@crans.org>
2016-04-05 00:37:44 +0200

    [gui/] removed unused panels, bug fix in number of remaining lives

--- src/main/scala/gui/infopanel.scala
+++ src/main/scala/gui/infopanel.scala
@@ -2,6 +2,7 @@
 package gui
 
 import swing._
+import Math._
 
 import game_mechanics.Player
 import runtime.{Controller,SpawnScheduler}
@@ -21,7 +22,7 @@ class InfoPanel() extends Panel
       (xm-2/85),2*ym/6+5)
     g.drawString("Gold : " + (Player.gold.toString),
       (xm/2-85),3*ym/6+5)
-    g.drawString("Lives : " + (Player.hp.toString),
+    g.drawString("Lives : " + ((Math.max(Player.hp, 0)).toString),
       (xm/2-85),4*ym/6+5)
     g.drawString("Bunnies slaughtered : "+ (Player.killcount.toString),
       (xm/2-85), 5*ym/6+5)
